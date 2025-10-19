c2c0736911d796727b28988b83b5171d7ed4b58d
Jose Ortega <josesantanas111110@gmail.com>
2025-10-18 17:46:00 -0700

    Descripcion de codigo

--- CALCULADORA.py
+++ CALCULADORA.py
@@ -1,4 +1,6 @@
 from colorama import Fore, Style
+
+#en este apartado se esta mostranto el titulo, por decirlo asi y el menu de nuestra calculadora.
 def menu():
     print(Fore.YELLOW+'===== PROYECTO FINAL PROGRAMACION====\n')
     print(Fore.CYAN+'      ***Calculadora JJ***')
@@ -8,6 +10,7 @@ def menu():
     print(Fore.BLUE+'4.Division')
     print(Fore.BLUE+'5.Salir')
 
+#en esta parte se pregunta al usuario que accion quiere realizar del menu, en caso de que seleccione el 5 esta por defaul que se termine el ciclo y deje "descansar el programa"
 def Calculadora():
     while True:
         menu()
@@ -15,7 +18,8 @@ def Calculadora():
         if opcion == '5':
             print(Fore.MAGENTA+'Gracias por dejarme descansar un poco💤, nos vemos hasta que me ocupes. ')
             break
-        
+
+#ya en esta parte se muestra la eleccion que hizo el ususario, ya sea suma, resta, multiplicacion y division        
         try:
             
             if opcion == '1':
@@ -26,14 +30,14 @@ def Calculadora():
                 print(Fore.GREEN+'Elegiste MULTIPLICACION✖️')
             if opcion == '4':
                 print(Fore.GREEN+'Elegiste DIVISION➗')
-
+#y aqui es donde nos pide los dos numeros a realizar la operacion seleccionada
             num1 = float(input('Ingresa el primer numero: '))
             num2 = float(input('Ingresa el segundo numero: '))
-        
+#en esta parte se genera una validacion o condicion que dice que nada mas se pueden ingresar numeros que esten en el menu        
         except ValueError:
             print(Fore.RED+'Introduce un numero valido')
             continue
-
+#y aqui ya es la impresion del resultado de la operacion elegida
         if opcion == '1':
             print('El resultado de tu SUMA es:', num1+num2)
         elif opcion == '2':
@@ -44,6 +48,7 @@ def Calculadora():
             try:
                 print('El resultado de tu DIVISION es', num1/num2)
             except ZeroDivisionError:
+#en caso de que se quiera dividir entre 0, y muestra un mensaje 
                 print('⚠️ No se puede DIVIDIR entre o, que no sabes😡 ⚠️ ')
         else:
             print('Opcion no valida')
